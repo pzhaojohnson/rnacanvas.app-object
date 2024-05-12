741954527527bb31aadbce6ab717b2cc88e8086d
Philip Johnson <p.zhao.johnson@gmail.com>
2024-05-12 11:17:41 -0400

    Update dependencies and remove drawDotBracket method

--- src/RNAcanvas.test.js
+++ src/RNAcanvas.test.js
@@ -51,22 +51,4 @@ describe('RNAcanvas class', () => {
     rnaCanvas.remove();
     expect(container.contains(rnaCanvas.domNode)).toBeFalsy();
   });
-
-  describe('drawDotBracket method', () => {
-    let rnaCanvas = new RNAcanvas();
-
-    rnaCanvas.drawDotBracket('AUCGUAGCUGCUGUGCUAGC', '..(((.....)))....');
-
-    let allBasesSorted = [...rnaCanvas.drawing.allBasesSorted];
-    let allSecondaryBonds = [...rnaCanvas.drawing.allSecondaryBonds];
-
-    it('appends a sequence of bases matching the given sequence', () => {
-      expect(allBasesSorted.map(b => b.textContent).join('')).toBe('AUCGUAGCUGCUGUGCUAGC');
-    });
-
-    it('appends secondary bonds corresponding to the base-pairs specified in the dot-bracket notation', () => {
-      expect(allSecondaryBonds.map(sb => allBasesSorted.indexOf(sb.base1))).toStrictEqual([2, 3, 4]);
-      expect(allSecondaryBonds.map(sb => allBasesSorted.indexOf(sb.base2))).toStrictEqual([12, 11, 10]);
-    });
-  });
 });
